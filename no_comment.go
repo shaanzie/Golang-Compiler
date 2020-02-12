de07818006286cae4d39c32e80eb0b8878fb68db
malavikka <malu2809@gmail.com>
2020-02-12 18:47:47 +0530

    identifiers for symbol table

--- no_comment.go
+++ no_comment.go
@@ -7,6 +7,7 @@ import "fmt"
 
 func main()
 {
+	a=10
 	return
 }
 
---
+++ var_input.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {  
+	var c int
+	var d float 
+	var e string
+    a:=true
+    {
+		a:="hello"
+	}
+	b:=30
+}
+
